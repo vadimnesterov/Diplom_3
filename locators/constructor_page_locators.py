5c942d81c5ad063084c8eef34ae932c2c122606e
Vadim Nesterov <vadimnesterov@hotmail.com>
2025-11-28 23:20:56 +0200

    constructor_page, constructor_page_locators

--- locators/constructor_page_locators.py
+++ locators/constructor_page_locators.py
@@ -1,11 +1,46 @@
+# locators/constructor_page_locators.py v1.1
+
 from selenium.webdriver.common.by import By
+
 
 class ConstructorPageLocators:
     PAGE_HEADER = (By.XPATH, "//h1[contains(text(),'Соберите бургер')]")
-    # --- TABS ---
-    TAB_BUNS = (By.XPATH, "//span[text()='Булки']/parent::div[@class='tab_tab__1SPyG pt-4 pr-10 pb-4 pl-10 noselect' or contains(@class,'tab_tab__')]")
-    TAB_SAUCES = (By.XPATH, "//span[text()='Соусы']/parent::div[contains(@class,'tab_tab__')]")
-    TAB_FILLINGS = (By.XPATH, "//span[text()='Начинки']/parent::div[contains(@class,'tab_tab__')]")
 
-    # Кнопка Оформить заказ
-    ORDER_BUTTON = (By.XPATH, "//button[contains(@class,'button_button') and .='Оформить заказ']")
+    # --- TABS ---
+    TAB_BUNS = (
+        By.XPATH,
+        "//span[text()='Булки']/parent::div[@class='tab_tab__1SPyG pt-4 pr-10 pb-4 pl-10 noselect' "
+        "or contains(@class,'tab_tab__')]"
+    )
+    TAB_SAUCES = (
+        By.XPATH,
+        "//span[text()='Соусы']/parent::div[contains(@class,'tab_tab__')]"
+    )
+    TAB_FILLINGS = (
+        By.XPATH,
+        "//span[text()='Начинки']/parent::div[contains(@class,'tab_tab__')]"
+    )
+
+    # --- INGREDIENTS ---
+    # Первый ингредиент в списке (бывший inline XPath из constructor_page.py)
+    FIRST_INGREDIENT = (
+        By.XPATH,
+        "//section[contains(@class,'BurgerIngredients')]"
+        "//*[self::li or self::a or self::div][1]"
+    )
+
+    # --- CONSTRUCTOR AREA ---
+    # Область конструктора для drag&drop (бывший inline XPath из constructor_page.py)
+    CONSTRUCTOR_DROP_AREA = (
+        By.XPATH,
+        "//main//section[contains(@class,'BurgerConstructor')]"
+        "//*[contains(@class,'BurgerConstructor_basket') "
+        "or contains(@class,'burger-element_pos_top') "
+        "or name()='ul']"
+    )
+
+    # --- ORDER BUTTON ---
+    ORDER_BUTTON = (
+        By.XPATH,
+        "//button[contains(@class,'button_button') and .='Оформить заказ']"
+    )
--- pages/constructor_page.py
+++ pages/constructor_page.py
@@ -1,17 +1,13 @@
-# constructor_page.py v1.1
+# pages/constructor_page.py v1.3
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
+from pages.base_page import BasePage
 from data.urls import MainUrl
 from locators.constructor_page_locators import ConstructorPageLocators
 
 
-class ConstructorPage:
+class ConstructorPage(BasePage):
     def __init__(self, driver):
-        self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        super().__init__(driver)
 
     # ----------------------------------------------------
     # Открытие конструктора
@@ -21,10 +17,8 @@ class ConstructorPage:
         Открывает страницу с конструктором бургера:
         переходим через базовый URL и ждём появления заголовка.
         """
-        self.driver.get(MainUrl.MAIN_URL)
-        self.wait.until(
-            EC.visibility_of_element_located(ConstructorPageLocators.PAGE_HEADER)
-        )
+        self.open(MainUrl.MAIN_URL)
+        self.wait_for_visible(ConstructorPageLocators.PAGE_HEADER)
 
     # ----------------------------------------------------
     # Вкладки: Булки / Соусы / Начинки
@@ -32,34 +26,29 @@ class ConstructorPage:
     def _wait_tab_active(self, locator):
         """
         Ожидает, что у вкладки появится класс "tab_tab_type_current".
+        Используем ожидание видимости.
         """
-        self.wait.until(
+        self.wait_for_condition(
             lambda d: "tab_tab_type_current"
-            in d.find_element(*locator).get_attribute("class")
+            in self.wait_for_visible(locator).get_attribute("class")
         )
 
     def select_buns_tab(self):
-        tab = self.wait.until(
-            EC.element_to_be_clickable(ConstructorPageLocators.TAB_BUNS)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", tab)
-        self.driver.execute_script("arguments[0].click();", tab)
+        tab = self.wait_for_clickable(ConstructorPageLocators.TAB_BUNS)
+        self.scroll_into_view(ConstructorPageLocators.TAB_BUNS)
+        self.click(ConstructorPageLocators.TAB_BUNS)
         self._wait_tab_active(ConstructorPageLocators.TAB_BUNS)
 
     def select_sauces_tab(self):
-        tab = self.wait.until(
-            EC.element_to_be_clickable(ConstructorPageLocators.TAB_SAUCES)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", tab)
-        self.driver.execute_script("arguments[0].click();", tab)
+        tab = self.wait_for_clickable(ConstructorPageLocators.TAB_SAUCES)
+        self.scroll_into_view(ConstructorPageLocators.TAB_SAUCES)
+        self.click(ConstructorPageLocators.TAB_SAUCES)
         self._wait_tab_active(ConstructorPageLocators.TAB_SAUCES)
 
     def select_fillings_tab(self):
-        tab = self.wait.until(
-            EC.element_to_be_clickable(ConstructorPageLocators.TAB_FILLINGS)
-        )
-        self.driver.execute_script("arguments[0].scrollIntoView({block:'center'});", tab)
-        self.driver.execute_script("arguments[0].click();", tab)
+        tab = self.wait_for_clickable(ConstructorPageLocators.TAB_FILLINGS)
+        self.scroll_into_view(ConstructorPageLocators.TAB_FILLINGS)
+        self.click(ConstructorPageLocators.TAB_FILLINGS)
         self._wait_tab_active(ConstructorPageLocators.TAB_FILLINGS)
 
     # ----------------------------------------------------
@@ -72,94 +61,24 @@ class ConstructorPage:
         Логика:
         - переключаемся на вкладку "Булки" (если возможно);
         - берём первый доступный ингредиент в списке;
-        - перетаскиваем его в область конструктора через JS drag&drop.
+        - перетаскиваем его в область конструктора через drag&drop из BasePage.
         """
-        # На всякий случай переключаемся на "Булки"
         try:
             self.select_buns_tab()
         except Exception:
-            # Если вкладка не переключилась — продолжаем, список обычно и так виден
             pass
 
-        # Ищем первый ингредиент в блоке ингредиентов
-        ingredient = self.wait.until(
-            EC.visibility_of_element_located(
-                (
-                    By.XPATH,
-                        "//section[contains(@class,'BurgerIngredients')]"
-                        "//*[self::li or self::a or self::div][1]",
-                )
-            )
+        # первый ингредиент
+        ingredient = self.wait_for_visible(
+            ConstructorPageLocators.FIRST_INGREDIENT
         )
 
-        # Ищем область конструктора, куда кидаем ингредиент
-        constructor_drop_area = self.wait.until(
-            EC.visibility_of_element_located(
-                (
-                    By.XPATH,
-                        "//main//section[contains(@class,'BurgerConstructor')]"
-                        "//*[contains(@class,'BurgerConstructor_basket') "
-                        "or contains(@class,'burger-element_pos_top') "
-                        "or name()='ul']",
-                )
-            )
+        # область конструктора
+        constructor_drop_area = self.wait_for_visible(
+            ConstructorPageLocators.CONSTRUCTOR_DROP_AREA
         )
 
-        self._drag_and_drop(ingredient, constructor_drop_area)
-
-    def _drag_and_drop(self, source_element, target_element):
-        """
-        Вспомогательный метод drag&drop на JS для перетаскивания ингредиента.
-        """
-        self.driver.execute_script(
-            """
-            function createEvent(typeOfEvent) {
-                var event = document.createEvent('CustomEvent');
-                event.initCustomEvent(typeOfEvent, true, true, null);
-                event.dataTransfer = {
-                    data: {},
-                    setData: function (key, value) {
-                        this.data[key] = value;
-                    },
-                    getData: function (key) {
-                        return this.data[key];
-                    }
-                };
-                return event;
-            }
-
-            function dispatchEvent(element, event, transferData) {
-                if (transferData !== undefined) {
-                    event.dataTransfer = transferData;
-                }
-                if (element.dispatchEvent) {
-                    element.dispatchEvent(event);
-                } else if (element.fireEvent) {
-                    element.fireEvent('on' + event.type, event);
-                }
-            }
-
-            var source = arguments[0];
-            var target = arguments[1];
-
-            var dragStartEvent = createEvent('dragstart');
-            dispatchEvent(source, dragStartEvent);
-
-            var dragEnterEvent = createEvent('dragenter');
-            dispatchEvent(target, dragEnterEvent, dragStartEvent.dataTransfer);
-
-            var dragOverEvent = createEvent('dragover');
-            dispatchEvent(target, dragOverEvent, dragStartEvent.dataTransfer);
-
-            var dropEvent = createEvent('drop');
-            dispatchEvent(target, dropEvent, dragStartEvent.dataTransfer);
-
-            var dragEndEvent = createEvent('dragend');
-            dispatchEvent(source, dragEndEvent, dragStartEvent.dataTransfer);
-            """,
-            source_element,
-            target_element,
-        )
+        self.drag_and_drop(ingredient, constructor_drop_area)
 
     # ----------------------------------------------------
     # Кнопка "Оформить заказ"
@@ -168,10 +87,5 @@ class ConstructorPage:
         """
         Ждём кликабельность кнопки "Оформить заказ" и кликаем по ней.
         """
-        button = self.wait.until(
-            EC.element_to_be_clickable(ConstructorPageLocators.ORDER_BUTTON)
-        )
-        self.driver.execute_script(
-            "arguments[0].scrollIntoView({block:'center'});", button
-        )
-        self.driver.execute_script("arguments[0].click();", button)
+        self.scroll_into_view(ConstructorPageLocators.ORDER_BUTTON)
+        self.click(ConstructorPageLocators.ORDER_BUTTON)
