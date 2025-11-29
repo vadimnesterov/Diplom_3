6e6538da9efda411b4ad3ab051dc751bcdb15ca0
Vadim Nesterov <vadimnesterov@hotmail.com>
2025-11-29 15:39:09 +0200

    login_page

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,24 +1,29 @@
-# pages/login_page.py v1.2
+# pages/login_page.py v1.4
+
+import allure
 
 from data.urls import MainUrl, URLS
 from .base_page import BasePage
 from locators.login_page_locators import LoginPageLocators
 from locators.constructor_page_locators import ConstructorPageLocators
-from data.urls import URLS
 
 
 class LoginPage(BasePage):
     """Страница логина."""
 
+    @allure.step("Открыть страницу логина")
     def open_login(self):
         self.open(MainUrl.MAIN_URL + URLS.url_login)
 
+    @allure.step("Ввести email")
     def set_email(self, email: str):
         self.type(LoginPageLocators.EMAIL_FIELD, email)
 
+    @allure.step("Ввести пароль")
     def set_password(self, password: str):
         self.type(LoginPageLocators.PASSWORD_FIELD, password)
 
+    @allure.step("Нажать кнопку 'Войти' и дождаться успешной авторизации")
     def submit_login(self) -> bool:
         """
         Нажать кнопку 'Войти' и дождаться успешной авторизации.
@@ -32,6 +37,7 @@ class LoginPage(BasePage):
             timeout=10
         )
 
+    @allure.step("Перейти на страницу регистрации")
     def go_to_register(self):
         """Переход на страницу регистрации."""
         self.click(LoginPageLocators.REGISTER_LINK)
